16eb3e1e953ca64c980511f91d5f503b13f7f265
martineho <martineolsenn@hotmail.com>
2021-11-06 14:15:09 +0100

    added css and js to home

--- js/home.js
+++ js/home.js
@@ -1,1 +1,34 @@
 const url = "http://martineho.com/travelcoco/wp-json/wp/v2/posts";
+const postsContainer = document.querySelector(".latestPosts");
+
+async function getPosts() {
+
+      const response = await fetch(url);
+  
+      const results = await response.json();
+  
+      console.log(results);
+
+      const posts = results;
+
+      postsContainer.innerHTML = "";
+
+      for (let i = 0; i < posts.length; i++) {
+
+        if (i === 8) {
+          break;
+        }
+  
+        postsContainer.innerHTML +=
+          `<div class="slider">
+          <a class="post">${posts[i].title}</a>
+          </div>`;
+      }
+  
+
+
+
+  
+}
+  
+getPosts();
