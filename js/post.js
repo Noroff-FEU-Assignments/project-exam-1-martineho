4f15cea5dde7fb38f9083b65acb8a9adcb48926a
martineho <martineolsenn@hotmail.com>
2021-12-01 14:06:46 +0100

    added css and js

--- js/post.js
+++ js/post.js
@@ -1,4 +1,3 @@
-const content = document.querySelector(".post-content");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
@@ -7,53 +6,52 @@ if (id === null) {
   location.href = "/";
 }
 
-
 const url = "http://martineho.com/travelcoco/wp-json/wp/v2/posts/" + id + "?_embed";
 const corsUrl = "https://noroffcors.herokuapp.com/" + url;
 const options = {};
 
+const container = document.querySelector(".container");
+const featured = document.querySelector(".featured-media");
+const modal = document.querySelector(".modal-container");
 // console.log(url);
 
 async function fetchPost() {
-  try {
+
     const response = await fetch(corsUrl, options);
-
     const result = await response.json();
-
     // console.log(result);
-
     const post = result;
 
     document.title = `${post.title.rendered}`;
 
-    content.innerHTML = "";
+    featured.innerHTML += ` <h1>${post.title.rendered}</h1>
 
-    content.innerHTML += `
+                            <div class="postDetails">
+                                  <a>By <span>${post._embedded["author"][0].name}</span></a>
+                                    <a>Published <span>${post.date}</span></a>
+                                  <a class="categoryTag" href="">${post._embedded["wp:term"][0][0].name}</a>
+                            </div>
+                            
+                            <img src="${post._embedded["wp:featuredmedia"][0].source_url}" class="featured-image"> `;
+
+    container.innerHTML = "";
+    container.innerHTML += `${post.content.rendered}`;
+
+    modal.innerHTML += `<span class="close">&times;</span>
+                        <img class="modal-content" id="modal-img">
+                        <div id="caption"></div>`;
+
+    // var img = document.querySelectorAll('figure img');  
+    //img.addEventListener("click", displayModal);
+    document.querySelectorAll(".img").onClick = displayModal;
+    
+    function displayModal (){
+      modal.style.display = "block";
+    }
+
   
-            <header>
-                <div class="breadcrumbs">
-                    <a href="blog.html">Go back to blog</a>
-              </div>
-
-                <h1>${post.title.rendered}</h1>
-                    <div class="postDetails">
-                        <a>By <span>${post._embedded["author"][0].name}</span></a>
-                        <a>Published <span>${post.date}</span></a>
-                        <a class="categoryTag" href="">${post._embedded["wp:term"][0][0].name}</a>
-                    </div>
-
-                <img src="${post._embedded["wp:featuredmedia"][0].source_url}" class="featured-image"> 
-            </header>
-    
-    
-            <section class="text">
-                <p>${post.content.rendered}</p>
-            </section>
-        `;
-      
-  } catch (error) {
-    console.log("An error occured");
-  }
 }
 
 fetchPost();
+
+
